6d02e8feeac74c40831f1d96c5d251e060fb7ec0
Magnus Bjerke Vik <mbvett@gmail.com>
2015-11-07 16:34:52 +0100

    Add 'post comment' api
    Be aware that this does not follow the current API specification. The spec
    will possibly be changed to follow this implementation.

--- server/resources.py
+++ server/resources.py
@@ -1,5 +1,6 @@
-from flask_restful import Resource, Api, abort
-from .models import Comment, Lecture
+from flask import request
+from flask_restful import Resource, Api, abort, reqparse
+from .models import db, Comment, Lecture
 
 api = Api()
 
@@ -22,5 +23,28 @@ class CommentListResource(Resource):
             'comments': comments
         }
 
+    def post(self, lecture_id):
+        db_lectures = Lecture.query.filter(Lecture.id == lecture_id).all()
+
+        if not db_lectures:
+            abort(404, message="Lecture {} does not exist".format(lecture_id))
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('content', help='Text content of comment')
+        args = parser.parse_args()
+
+        if not args.content:
+            abort(400, message="Comment has no content parameter")
+
+        content = args.content
+        lecture = db_lectures[0]
+
+        comment = Comment(content, lecture)
+        db.session.add(comment)
+        db.session.commit()
+
+        return {
+            'id': comment.id
+        }
 
 api.add_resource(CommentListResource, '/api/0/lectures/<lecture_id>/comments')
