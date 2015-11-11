23e0b92392e7eb69b41e37d5697ac8ca27fdf21d
121052 <thomas.mellemseter@hig.no>
2015-11-11 15:07:57 +0100

    Fix refresh issue (loading comments)

--- client/student/app/lecture/lecture.factory.js
+++ client/student/app/lecture/lecture.factory.js
@@ -6,12 +6,9 @@
         .factory('lectureFactory', lectureFactory);
 
     /* @ngInject */
-    function lectureFactory(lectureService, $location) {
+    function lectureFactory(lectureService, $location, $routeParams) {
         console.log('Ready (Lecture Factory)');
-        var lectureId;
-        
-        // NOTE: This object needs to be stored locally 
-        // in order to load a joined lecture on page refresh.
+        var lectureId = $routeParams.lectureId;
         var lecture = {};
     
         var service = {
@@ -24,20 +21,17 @@
         return service;
         
         function joinLecture(id, error) {
-
             lectureId = id;
-            
             lectureService.retrieveLecture
                 .get({lectureId: lectureId}, onSuccess, onFail);
             
             function onSuccess(response) {
                 console.log('Success');
                 lecture = response;
-                $location.path('/lecture/' + id);
+                $location.path('/lecture/' + lectureId);
             }
             
             function onFail(response) {
-                console.log('Error: ', response.message);
                 
                 // Show user feedback.
                 if (angular.isFunction(error)) {
@@ -46,6 +40,7 @@
                 
                 // When manually typing the route with an id,
                 // Redirect user to default route.
+                //$location.path('/');
                 $location.path('/');
             }
         }
@@ -64,10 +59,9 @@
         }
         
         function getComments(onSuccess, onFail) {
-            if (angular.isDefined(lectureId)) {
-                lectureService.retrieveComments
-                    .get({lectureId: lectureId}, onSuccess, onFail);
-            }
+            lectureService.retrieveComments
+                .get({lectureId: lectureId}, onSuccess, onFail);
+            
         }
     }
 })();
