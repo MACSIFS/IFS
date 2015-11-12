ce3288a0056354bf6ed537c4387ed986db9a88cd
Magnus Bjerke Vik <mbvett@gmail.com>
2015-11-12 20:54:32 +0100

    Make server generate a unique client_id cookie for each client

---
+++ server/tests/test_clientid.py
@@ -0,0 +1,35 @@
+from server.tests.base import BaseTestCase
+
+
+class ClientIdTest(BaseTestCase):
+    def _find_client_id_cookie(self):
+        client_id = None
+        for cookie in self.client.cookie_jar:
+            if cookie.name == 'client_id':
+                client_id = cookie.value
+        return client_id
+
+    def test_generate_id(self):
+        rv = self.client.get('/')
+
+        client_id = self._find_client_id_cookie()
+
+        assert client_id is not None
+
+    def test_reuse_id(self):
+        rv = self.client.get('/')
+
+        client_id_1 = self._find_client_id_cookie()
+        client_id_2 = self._find_client_id_cookie()
+
+        assert client_id_1 == client_id_2
+
+    def test_clear_cookie(self):
+        rv = self.client.get('/')
+
+        client_id_1 = self._find_client_id_cookie()
+        self.client.cookie_jar.clear()
+        client_id_2 = self._find_client_id_cookie()
+
+        assert client_id_1 != client_id_2
+
