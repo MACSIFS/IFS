b77eba799738a81a0d2b7698f74613dfd0a202cc
121052 <thomas.mellemseter@hig.no>
2015-11-19 16:18:25 +0100

    IFS-71: Front end engagement for lecturer
    Include directive with service and factory according to the API
    specifications for Get Engagements.

--- client/lecturer/app/engagement.directive.js
+++ client/lecturer/app/engagement.directive.js
@@ -21,31 +21,45 @@
 
             engagementCanvas.canvas = element[0];
             engagementCanvas.ctx = engagementCanvas.canvas.getContext('2d');
-            engagementCanvas.drawBackground(30);
-            engagementCanvas.drawDot(0.0, 0.0, 30);
+
+            engagementCanvas.drawBackground(engagementCanvas.padding);
+            engagementCanvas.pollEngagements(1000);
         }
     }
 
     /* @ngInject */
-    function EngagementCanvasController() {
+    function EngagementCanvasController(lecturesFactory) {
         console.log('Ready (Engagement Canvas Controller)');
 
         var engagementCanvas = this;
+        engagementCanvas.padding = 30;
         engagementCanvas.canvas = {};
         engagementCanvas.ctx = {};
 
         engagementCanvas.drawBackground = drawBackground;
         engagementCanvas.drawDot = drawDot;
+        engagementCanvas.pollEngagements = pollEngagements;
 
         function drawBackground(padding) {
+            engagementCanvas.ctx.clearRect(
+                0,
+                0,
+                engagementCanvas.canvas.width,
+                engagementCanvas.canvas.height
+            );
+
             engagementCanvas.ctx.beginPath();
             engagementCanvas.ctx.moveTo(padding,padding);
-            engagementCanvas.ctx.lineTo(padding,400 - padding);
-            engagementCanvas.ctx.lineTo(400 - padding, 400 - padding);
+            engagementCanvas.ctx.lineTo(padding, engagementCanvas.canvas.height - padding);
+            engagementCanvas.ctx.lineTo(
+                engagementCanvas.canvas.width - padding,
+                engagementCanvas.canvas.height - padding
+            );
             engagementCanvas.ctx.lineWidth = 3;
             engagementCanvas.ctx.lineCap = 'round';
             engagementCanvas.ctx.stroke();
 
+            engagementCanvas.ctx.fillStyle = 'black';
             engagementCanvas.ctx.textAlign = 'center';
             engagementCanvas.ctx.font = '18pt Calibri';
             engagementCanvas.ctx.fillText('Interest', 200, 390);
@@ -57,21 +71,38 @@
 
         function drawDot(x, y, padding) {
             if (x >= 0 && x <= 1 && y >= 0 && y <= 1) {
-
                 var cartesianWidth = (engagementCanvas.canvas.width - 2*padding);
                 var cartesianHeight = (engagementCanvas.canvas.height - 2*padding);
-
+                var drawableX = (cartesianWidth*x + padding);
+                var drawableY = (engagementCanvas.canvas.width - (cartesianHeight*y + padding));
+                
                 engagementCanvas.ctx.beginPath();
                 engagementCanvas.ctx.fillStyle = 'rgba(0,0,0,0.5)';
-                engagementCanvas.ctx.arc(
-                    (cartesianWidth*x + padding),
-                    (engagementCanvas.canvas.width - (cartesianHeight*y + padding)),
-                    10,
-                    0,
-                    2*Math.PI
-                );
+                engagementCanvas.ctx.arc(drawableX, drawableY, 10, 0, 2*Math.PI);
                 engagementCanvas.ctx.fill();
             }
+        }
+        
+        function drawDots(engagements) {
+            angular.forEach(engagements, function(engagement) {
+                drawDot(
+                    engagement.interest,
+                    engagement.challenge,
+                    engagementCanvas.padding
+                );
+            });
+        }
+        
+        function pollEngagements(interval) {
+            setInterval(function() {
+
+                lecturesFactory
+                    .getEngagements(function(response) {
+                        drawBackground(engagementCanvas.padding);
+                        drawDots(response.engagements);
+                    });
+
+            }, interval);
         }
     }
 })();
---
+++ client/lecturer/app/lectures.factory.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+    
+    angular
+        .module('lecturer')
+        .factory('lecturesFactory', lecturesFactory);
+
+    /* @ngInject */
+    function lecturesFactory(lecturesService) {
+        var lectureId = 1;
+        var service = {
+            getEngagements: getEngagements
+        };
+        
+        return service;
+        
+        function getEngagements(onSuccess) {
+            lecturesService.getEngagements
+                .get({lectureId: lectureId, latest: true}, onSuccess, onError);
+
+            function onError() {
+                console.log('Error');
+            }
+        }
+    }
+})();
