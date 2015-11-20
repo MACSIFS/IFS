cef6e38eedac910ccbc4ff5a2ff9009ead0dabfe
Nikita Uvarov <uv.nikita@gmail.com>
2015-11-20 16:26:07 +0100

    Implement production deployment
    deploy/setup_production.sh script should be used

---
+++ deploy/setup_production.sh
@@ -0,0 +1,7 @@
+SRC_DIR = $1
+
+apt-get update -qq
+apt-get install -y python-pip
+pip install ansible
+
+ansible-playbook "$SRC_DIR/deploy/setup_server.yml" -v -i 'localhost,' --extra-vars "src_dir=$SRC_DIR os=linux deploy_type=production" --connection=local
