9be1c1f37a9a4046587b969e3e076cb51889d381
Nikita Uvarov <uv.nikita@gmail.com>
2015-11-21 14:51:34 +0100

    Changed sha1 to sha256

--- server/auth/util.py
+++ server/auth/util.py
@@ -1,7 +1,10 @@
-from _sha1 import sha1
+from _sha256 import sha256
 
 from flask import current_app
 
 
 def hash_password(password):
-    return sha1(password.encode() + current_app.config['SECRET_KEY'].encode()).hexdigest()
+    return (
+        sha256(password.encode() + current_app.config['SECRET_KEY'].encode())
+        .hexdigest()
+    )
