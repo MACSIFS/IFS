1230b3d58c96ae3e76bb10003019dc7128cb7d2c
Jan Greger Hemb <jan.greger@gmail.com>
2015-11-24 12:35:33 +0100

    Old solution not working. Changed to prevent summation if exceeding maxlength.

--- client/student/app/lecture/comment.form.directive.js
+++ client/student/app/lecture/comment.form.directive.js
@@ -35,25 +35,38 @@
         
         function submitComment() {
             commentForm.submitted = true;
-            lectureFactory.submitComment(commentForm.comment, function(response) {
-                console.log('Success');
-                commentForm.feedbackMessage = 'Comment submitted!';
-                commentForm.feedbackType = 'alert-success';
-                commentForm.feedbackIcon = 'glyphicon-ok-sign';
-                
-                var comment = {
-                    id: response.id,
-                    content: commentForm.comment,
-                    submissionTime: new Date()
-                };
-                
-                commentForm.list.push(comment);
-            }, function() {
+            
+            var commentMaxLength = 500;
+            
+            if(commentForm.comment.length > commentMaxLength){
                 console.log('error');
-                commentForm.feedbackMessage = 'An error occured!';
+                commentForm.feedbackMessage = 'Comment/question too long!';
                 commentForm.feedbackType = 'alert-danger';
                 commentForm.feedbackIcon = 'glyphicon-exclamation-sign';
-            });
+            }else{
+            
+                lectureFactory.submitComment(commentForm.comment, function(response) {
+                    console.log('Success');
+                    commentForm.feedbackMessage = 'Comment submitted!';
+                    commentForm.feedbackType = 'alert-success';
+                    commentForm.feedbackIcon = 'glyphicon-ok-sign';
+                    
+                    var comment = {
+                        id: response.id,
+                        content: commentForm.comment,
+                        submissionTime: new Date()
+                    };
+                    
+                    commentForm.list.push(comment);
+                }, function() {
+                    console.log('error');
+                    commentForm.feedbackMessage = 'An error occured!';
+                    commentForm.feedbackType = 'alert-danger';
+                    commentForm.feedbackIcon = 'glyphicon-exclamation-sign';
+                });
+            
+            }
+            
         }
     }    
 })();
