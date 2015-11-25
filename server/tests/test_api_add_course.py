9e196f8d965ea10a32ff876a9d13fadc71901845
Magnus Bjerke Vik <mbvett@gmail.com>
2015-11-25 18:05:20 +0100

    Use variables in stead of hardcoded values in 'add course' API

--- server/tests/test_api_add_course.py
+++ server/tests/test_api_add_course.py
@@ -1,31 +1,27 @@
 import json
-from uuid import uuid1
-from datetime import datetime, timedelta
-
-import dateutil.parser
 
 from server.tests.base import BaseTestCase
-from server.models import db, Lecturer, Course, Lecture, Comment, Engagement
+from server.models import db, Lecturer, Course, Lecture
 
 
 class AddCourseApiTest(BaseTestCase):
     def setUp(self):
         super(AddCourseApiTest, self).setUp()
 
-        simon = Lecturer('simon', '1234', 'Simon', 'McCallum')
-        db.session.add(simon)
+        self.simon = Lecturer('simon', '1234', 'Simon', 'McCallum')
+        db.session.add(self.simon)
 
-        imt3601 = Course('IMT3601 - Game Programming', simon)
+        imt3601 = Course('IMT3601 - Game Programming', self.simon)
         db.session.add(imt3601)
 
         imt3601_l1 = Lecture('Lecture 1', imt3601)
         db.session.add(imt3601_l1)
 
-        frode = Lecturer('frode', '1234', 'Frode', 'Haug')
-        db.session.add(frode)
+        self.frode = Lecturer('frode', '1234', 'Frode', 'Haug')
+        db.session.add(self.frode)
 
-        imt1031 = Course('IMT1031 - Grunnleggende Programmering', frode)
-        db.session.add(frode)
+        imt1031 = Course('IMT1031 - Grunnleggende Programmering', self.frode)
+        db.session.add(imt1031)
 
         imt1031_l1 = Lecture('Lecture 1', imt1031)
         db.session.add(imt1031_l1)
@@ -33,17 +29,17 @@ class AddCourseApiTest(BaseTestCase):
         db.session.commit()
 
     def test_wrong_lecturer(self):
-        self.login('simon', '1234')
+        self.login(self.simon.email, self.simon.password)
 
         res = self.client.post('/api/0/courses', data=dict(
-            lecturerId=2,
+            lecturerId=self.frode.id,
             name='awsm'
         ))
 
         self.assert403(res)
 
     def test_no_lecturer(self):
-        self.login('simon', '1234')
+        self.login(self.simon.email, self.simon.password)
 
         res = self.client.post('/api/0/courses', data=dict(
             name='awsm'
@@ -52,19 +48,19 @@ class AddCourseApiTest(BaseTestCase):
         self.assert400(res)
 
     def test_no_name(self):
-        self.login('simon', '1234')
+        self.login(self.simon.email, self.simon.password)
 
         res = self.client.post('/api/0/courses', data=dict(
-            lecturerId=1
+            lecturerId=self.simon.id
         ))
 
         self.assert400(res)
 
     def test_success(self):
-        self.login('simon', '1234')
+        self.login(self.simon.email, self.simon.password)
 
         res = self.client.post('/api/0/courses', data=dict(
-            lecturerId=1,
+            lecturerId=self.simon.id,
             name='awsm'
         ))
         self.assert200(res)
@@ -78,4 +74,4 @@ class AddCourseApiTest(BaseTestCase):
         awsmCourse = Course.query.filter(Course.id == 3).first()
         self.assertFalse(awsmCourse is None)
         self.assertEqual(awsmCourse.name, 'awsm')
-        self.assertEqual(awsmCourse.lecturer_id, 1)
+        self.assertEqual(awsmCourse.lecturer_id, self.simon.id)
