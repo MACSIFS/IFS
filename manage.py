1ec1b07c6a4edb5ad9ecf930e8ac832d9cae0705
Magnus Bjerke Vik <mbvett@gmail.com>
2015-11-26 13:35:00 +0100

    Set different times on the mocked comments
    Now able to see if time sorting actually works

--- manage.py
+++ manage.py
@@ -34,28 +34,31 @@ def mock_db():
     imt3601_l1 = Lecture('Lecture 1', imt3601)
     db.session.add(imt3601_l1)
 
-    imt3601_l1_c1 = Comment('This is boring', datetime.utcnow(), imt3601_l1)
+    imt3601_l1_c1 = Comment('This is boring', datetime(2015, 11, 26, 10, 30), imt3601_l1)
     db.session.add(imt3601_l1_c1)
-    imt3601_l1_c2 = Comment('This is fun!', datetime.utcnow(), imt3601_l1)
+    imt3601_l1_c2 = Comment('This is fun!', datetime(2015, 11, 26, 10, 40), imt3601_l1)
     db.session.add(imt3601_l1_c2)
-    imt3601_l1_c3 = Comment('Help?', datetime.utcnow(), imt3601_l1)
+    imt3601_l1_c3 = Comment('Help?', datetime(2015, 11, 26, 10, 50), imt3601_l1)
     db.session.add(imt3601_l1_c3)
-    imt3601_l1_c4 = Comment('A bit longer comment for your convenience', datetime.utcnow(), imt3601_l1)
+    imt3601_l1_c4 = Comment(
+        'A bit longer comment for your convenience',
+        datetime(2015, 11, 26, 10, 10),
+        imt3601_l1)
     db.session.add(imt3601_l1_c4)
-    
+
     imt3601_l1_r1 = CommentRating(1, 1, imt3601_l1_c1, imt3601_l1)
     db.session.add(imt3601_l1_r1)
     imt3601_l1_r2 = CommentRating(1, 2, imt3601_l1_c1, imt3601_l1)
     db.session.add(imt3601_l1_r2)
     imt3601_l1_r3 = CommentRating(1, 3, imt3601_l1_c1, imt3601_l1)
     db.session.add(imt3601_l1_r3)
-    
+
     imt3601_l1_r4 = CommentRating(-1, 1, imt3601_l1_c3, imt3601_l1)
     db.session.add(imt3601_l1_r4)
-    
+
     imt3601_l1_r5 = CommentRating(1, 1, imt3601_l1_c4, imt3601_l1)
     db.session.add(imt3601_l1_r5)
-    
+
 
     db.session.commit()
 
